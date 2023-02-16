1c5ec400ca40cbed2c96ab5df8650a13296129c8
Allyn L. Bottorff <abottorff@gmail.com>
2023-02-15 22:29:31 -0600

    wip cloudflare

--- cloudflare/cloudflare.go
+++ cloudflare/cloudflare.go
@@ -1,19 +1,47 @@
 package cloudflare
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
 var CloudFlareZonesURL string = "https://api.cloudflare.com/client/v4/zones"
 
-
-type CloudflareCreds struct {
+type creds struct {
 	Token string `json:"token"`
 }
 
-
-type CloudflareZoneResult struct {
-	Result []CloudflareZone `json:"result"`
+type zoneResult struct {
+	Result []zone `json:"result"`
 }
 
-type CloudflareZone struct {
-Name string `json:"name"`
-ID string `json:"id"`
+type zone struct {
+	Name string `json:"name"`
+	ID   string `json:"id"`
+}
 
+func GetZones(token string) []zone{
+
+	client := &http.Client{}
+	request, err := http.NewRequest(http.MethodGet, CloudFlareZonesURL, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	request.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
+	resp, err := client.Do(request)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var zr zoneResult
+
+	err = json.NewDecoder(resp.Body).Decode(&zr)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return zr.Result
 }

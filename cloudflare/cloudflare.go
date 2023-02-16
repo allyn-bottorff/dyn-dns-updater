7a08130c2221b3860a70de10e927a381e488c7e3
Allyn L. Bottorff <allyn.bottorff@veteransunited.com>
2023-02-16 13:45:05 -0600

    added getApex

--- cloudflare/cloudflare.go
+++ cloudflare/cloudflare.go
@@ -22,7 +22,21 @@ type zone struct {
 	ID   string `json:"id"`
 }
 
-func GetZones(token string) []zone{
+type zoneDNSResponse struct {
+	Success bool     `json:"success"`
+	Result  []record `json:"result"`
+}
+
+type record struct {
+	ID      string `json:"id"`
+	Type    string `json:"type"`
+	Name    string `json:"name"`
+	Content string `json:"content"`
+}
+
+
+// Get all of the DNS zones which are accessible by the token.
+func GetZones(token string) []zone {
 
 	client := &http.Client{}
 	request, err := http.NewRequest(http.MethodGet, CloudFlareZonesURL, nil)
@@ -44,4 +58,36 @@ func GetZones(token string) []zone{
 	}
 
 	return zr.Result
+}
+
+
+// Get the apex A record of a zone
+func getApex(z zone) (string, error) {
+	var ipAddr string
+	var zoneDNS zoneDNSResponse
+	client := &http.Client{}
+	request, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/%s/dns_records", CloudFlareZonesURL, z.ID), nil)
+	if err != nil {
+		return ipAddr, err
+	}
+	resp, err := client.Do(request)
+	if err != nil {
+		return ipAddr, err
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&zoneDNS)
+	if err != nil {
+		return ipAddr, err
+	}
+
+	for _, record := range zoneDNS.Result {
+		if record.Name == z.Name {
+			if record.Type == "A" {
+				ipAddr = record.Content
+				break
+			}
+		}
+	}
+	return ipAddr, err
+
 }

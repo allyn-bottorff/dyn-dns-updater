bc737094f51b45f969a472bff12fb7ab0abeedac
Allyn L. Bottorff <abottorff@gmail.com>
2023-02-26 20:47:43 -0600

    working rust version

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,7 +1,7 @@
 use std::net::Ipv4Addr;
 use std::str::FromStr;
 use std::{error, fs};
-// use std::{thread, time};
+use std::{thread, time};
 
 use reqwest;
 use serde::{Deserialize, Serialize};
@@ -11,6 +11,13 @@ use serde_json;
 struct UnifiCreds {
     username: String,
     password: String,
+}
+
+#[derive(Deserialize)]
+struct Config {
+    unifi: UnifiCreds,
+    cftoken: String,
+    watch_records: Vec<String>,
 }
 
 #[derive(Deserialize)]
@@ -24,7 +31,7 @@ struct SubSystemHealth {
     wan_ip: Option<String>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 struct Zone {
     name: String,
     id: String,
@@ -37,7 +44,7 @@ struct ZoneResult {
 
 #[derive(Deserialize)]
 struct ZoneDNSResponse {
-    success: bool,
+    // success: bool,
     result: Vec<Record>,
 }
 
@@ -58,16 +65,12 @@ type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
 /// -- If different, update zone apex records in Cloudflare
 /// Sleep 10 minutes or so.
 fn main() {
-    println!("Starting IP Sync...");
-
-    // loop {
-
     let mut u_client = reqwest::blocking::Client::builder()
         .cookie_store(true)
         .build()
         .unwrap();
 
-    let u_creds = match read_secrets() {
+    let conf = match read_secrets() {
         Ok(c) => c,
         Err(e) => {
             println!("{}", e);
@@ -75,62 +78,116 @@ fn main() {
             // continue;
         }
     };
-    let body = match serde_json::to_string(&u_creds) {
-        Ok(b) => b,
-        Err(e) => {
-            println!("{}", e);
-            panic!();
-            // continue;
-        }
-    };
-    let resp = match u_client
-        .post("https://unifi.b6f.net/api/login")
-        .body(body)
-        .send()
-    {
-        Ok(r) => r,
-        Err(e) => {
-            println!("{}", e);
-            panic!();
-            // continue;
-        }
-    };
 
-    if resp.status() != reqwest::StatusCode::OK {
-        println!("UniFi login failed: {}", resp.status().to_string());
-        panic!();
-        // continue;
-    }
+    // Loop forever with a thread sleep
+    loop {
+        thread::sleep(time::Duration::new(30, 0));
 
-    println!("Unifi login successsful");
-
-    let wanip = match get_unifi_ip(&mut u_client) {
-        Ok(opt) => match opt {
-            Some(ip) => ip,
-            None => {
-                println!("Failed to parse IP from unifi");
+        println!("Starting IP Sync...");
+        let body = match serde_json::to_string(&conf.unifi) {
+            Ok(b) => b,
+            Err(e) => {
+                println!("{}", e);
                 panic!();
                 // continue;
             }
-        },
-        Err(e) => {
-            println!("{}", e);
-            panic!();
-            // continue;
+        };
+        let resp = match u_client
+            .post("https://unifi.b6f.net/api/login")
+            .body(body)
+            .send()
+        {
+            Ok(r) => r,
+            Err(e) => {
+                println!("{}", e);
+                // panic!();
+                continue;
+            }
+        };
+
+        if resp.status() != reqwest::StatusCode::OK {
+            println!("UniFi login failed: {}", resp.status().to_string());
+            // panic!();
+            continue;
         }
-    };
 
-    println!("WAN IP: {}", wanip.to_string());
+        println!("Unifi login successsful");
 
-    // thread::sleep(time::Duration::new(30, 0));
-    // }
+        let wanip = match get_unifi_ip(&mut u_client) {
+            Ok(opt) => match opt {
+                Some(ip) => ip,
+                None => {
+                    println!("Failed to parse IP from unifi");
+                    // panic!();
+                    continue;
+                }
+            },
+            Err(e) => {
+                println!("{}", e);
+                // panic!();
+                continue;
+            }
+        };
+
+        println!("WAN IP: {}", wanip.to_string());
+
+        let c_client = reqwest::blocking::Client::new();
+
+        let zones = match get_zones(&c_client, &conf.cftoken) {
+            Ok(z) => z,
+            Err(e) => {
+                println!("{}", e);
+                // panic!();
+                continue;
+            }
+        };
+
+        // println!("Cloudflare Zones:");
+        // println!("{:?}", zones);
+
+        for zone in zones {
+            if conf.watch_records.contains(&zone.name) {
+                println!("Checking zone: {}", zone.name);
+                let apex = match get_apex(&zone, &c_client, &conf.cftoken) {
+                    Ok(opt) => match opt {
+                        Some(apex) => apex,
+                        None => {
+                            println!("Failed to get apex A record for zone: {}", zone.name);
+                            // panic!();
+                            continue;
+                        }
+                    },
+                    Err(e) => {
+                        println!("{}", e);
+                        // panic!();
+                        continue;
+                    }
+                };
+                println!("Zone apex: {}", apex.content);
+                if apex.content != wanip.to_string() {
+                    println!("Found mismatched zone: {}", zone.name);
+                    match update_apex(&zone, &apex, &wanip, &c_client, &conf.cftoken) {
+                        Ok(()) => {
+                            println!("Updated {} with IP: {}", &zone.name, wanip.to_string());
+                        }
+                        Err(e) => {
+                            println!("{}", e);
+                            // panic!();
+                            continue;
+                        }
+                    };
+                    //update record
+                }
+            }
+        }
+    }
 }
 
 /// Read Unifi login credentials from a file
-fn read_secrets() -> Result<UnifiCreds> {
+fn read_secrets() -> Result<Config> {
     let contents = fs::read_to_string("./creds.json")?;
 
-    let creds: UnifiCreds = serde_json::from_str(&contents)?;
+    let creds: Config = serde_json::from_str(&contents)?;
 
     Ok(creds)
 }
@@ -139,7 +196,8 @@ fn read_secrets() -> Result<UnifiCreds> {
 fn get_unifi_ip(client: &mut reqwest::blocking::Client) -> Result<Option<Ipv4Addr>> {
     let resp = client
         .get("https://unifi.b6f.net/api/s/default/stat/health")
-        .send()?;
+        .send()?
+        .error_for_status()?;
 
     let health: UnifiHealth = resp.json()?;
 
@@ -159,4 +217,64 @@ fn get_unifi_ip(client: &mut reqwest::blocking::Client) -> Result<Option<Ipv4Add
 }
 
 /// Get the zones from CloudFlare
-fn get_zone(token: String) {}
+fn get_zones(client: &reqwest::blocking::Client, token: &String) -> Result<Vec<Zone>> {
+    let resp = client
+        .get("https://api.cloudflare.com/client/v4/zones")
+        .header("Authorization", format!("Bearer {}", token))
+        .send()?
+        .error_for_status()?;
+
+    let zone_result: ZoneResult = resp.json()?;
+
+    Ok(zone_result.result)
+}
+
+/// Get the apex A record from a zone
+fn get_apex(
+    zone: &Zone,
+    client: &reqwest::blocking::Client,
+    token: &String,
+) -> Result<Option<Record>> {
+    let resp = client
+        .get(format!(
+            "https://api.cloudflare.com/client/v4/zones/{}/dns_records",
+            zone.id
+        ))
+        .header("Authorization", format!("Bearer {}", token))
+        .send()?
+        .error_for_status()?;
+
+    let z_result: ZoneDNSResponse = resp.json()?;
+
+    for rec in z_result.result {
+        if rec.name == zone.name {
+            if rec.r#type == "A" {
+                return Ok(Some(rec));
+            }
+        }
+    }
+    Ok(None)
+}
+
+/// Update an A record
+fn update_apex(
+    zone: &Zone,
+    record: &Record,
+    wanip: &Ipv4Addr,
+    client: &reqwest::blocking::Client,
+    token: &String,
+) -> Result<()> {
+    let body = format!("{{\"content\": \"{}\"}}", wanip.to_string());
+    let _resp = client
+        .patch(format!(
+            "https://api.cloudflare.com/client/v4/zones/{}/dns_records/{}",
+            zone.id, record.id
+        ))
+        .header("Authorization", format!("Bearer {}", token))
+        .header("Content-Type", "application/json")
+        .body(body)
+        .send()?
+        .error_for_status()?;
+
+    Ok(())
+}
